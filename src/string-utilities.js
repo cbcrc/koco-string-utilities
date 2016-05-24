720991a5c260dab7ccb264faaa2775fb32e99248
Maxime Forget-Séguin <maxime.forget-seguin@radio-canada.ca>
2016-05-24 13:23:19 -0400

    update to koco2

--- src/string-utilities.js
+++ src/string-utilities.js
@@ -1,79 +1,77 @@
 // Copyright (c) CBC/Radio-Canada. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-define(['slug'],
-    function(slug) {
-        'use strict';
+import slug from 'slug';
 
-        var StringUtilities = function() {
-            this.slugOptions = {
-                lower: true,
-                symbols: false,
-                multicharmap: {}
-            };
-        };
 
-        StringUtilities.prototype.equalsIgnoreCase = function(str1, str2) {
-            str1 = str1 || '';
-            str2 = str2 || '';
-            return str1.toLowerCase() === str2.toLowerCase();
-        };
+var StringUtilities = function() {
+    this.slugOptions = {
+        lower: true,
+        symbols: false,
+        multicharmap: {}
+    };
+};
 
-        StringUtilities.prototype.caseInsensitiveCmp = function(left, right) {
-            return this.equalsIgnoreCase(left, right);
-        };
+StringUtilities.prototype.equalsIgnoreCase = function(str1, str2) {
+    str1 = str1 || '';
+    str2 = str2 || '';
+    return str1.toLowerCase() === str2.toLowerCase();
+};
 
-        StringUtilities.prototype.startsWithIgnoreCase = function(str1, str2) {
-            str1 = str1 || '';
-            str2 = str2 || '';
-            return this.equalsIgnoreCase(str1.substr(0, str2.length), str2);
-        };
+StringUtilities.prototype.caseInsensitiveCmp = function(left, right) {
+    return this.equalsIgnoreCase(left, right);
+};
 
-        StringUtilities.prototype.capitaliseFirstLetter = function(value) {
-            return value.charAt(0).toUpperCase() + value.slice(1);
-        };
+StringUtilities.prototype.startsWithIgnoreCase = function(str1, str2) {
+    str1 = str1 || '';
+    str2 = str2 || '';
+    return this.equalsIgnoreCase(str1.substr(0, str2.length), str2);
+};
 
-        StringUtilities.prototype.uncapitaliseFirstLetter = function(value) {
-            return value.charAt(0).toLowerCase() + value.slice(1);
-        };
+StringUtilities.prototype.capitaliseFirstLetter = function(value) {
+    return value.charAt(0).toUpperCase() + value.slice(1);
+};
 
-        StringUtilities.prototype.stripHtmlFromText = function(text) {
-            return text.replace(/&nbsp;/g, ' ').replace(/<(?:.|\n)*?>/g, '');
-        };
+StringUtilities.prototype.uncapitaliseFirstLetter = function(value) {
+    return value.charAt(0).toLowerCase() + value.slice(1);
+};
 
-        StringUtilities.prototype.containsHtmlInText = function(text) {
-            var nbsps = text.match(/&nbsp;/g);
-            var tags = text.match(/<(?:.|\n)*?>/g);
+StringUtilities.prototype.stripHtmlFromText = function(text) {
+    return text.replace(/&nbsp;/g, ' ').replace(/<(?:.|\n)*?>/g, '');
+};
 
-            return nbsps || tags;
-        };
+StringUtilities.prototype.containsHtmlInText = function(text) {
+    var nbsps = text.match(/&nbsp;/g);
+    var tags = text.match(/<(?:.|\n)*?>/g);
 
-        StringUtilities.prototype.trimRight = function(string, charlist) {
-            if (string) {
-                if (string.toString) {
-                    string = string.toString();
-                }
+    return nbsps || tags;
+};
 
-                if (charlist === undefined) {
-                    charlist = '\s';
-                }
+StringUtilities.prototype.trimRight = function(string, charlist) {
+    if (string) {
+        if (string.toString) {
+            string = string.toString();
+        }
 
-                return string.replace(new RegExp('[' + charlist + ']+$'), '');
-            }
+        if (charlist === undefined) {
+            charlist = '\s';
+        }
 
-            return '';
-        };
+        return string.replace(new RegExp('[' + charlist + ']+$'), '');
+    }
 
-        StringUtilities.prototype.toSlug = function(text) {
-            if (!text) {
-                return '';
-            }
+    return '';
+};
 
-            text = this.stripHtmlFromText(text);
-            text = text.replace(/&amp;/g, '&');
+StringUtilities.prototype.toSlug = function(text) {
+    if (!text) {
+        return '';
+    }
 
-            return slug(text, this.slugOptions);
-        };
+    text = this.stripHtmlFromText(text);
+    text = text.replace(/&amp;/g, '&');
 
-        return new StringUtilities();
-    });
+    return slug(text, this.slugOptions);
+};
+
+export default new StringUtilities();
